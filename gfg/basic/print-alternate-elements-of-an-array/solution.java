d37f3fd10ea0138bdd84393b8c8d6ab9bac0679f
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-08-13 22:30:17 +0530

    ✅ Alternates in an Array | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ gfg/basic/print-alternate-elements-of-an-array/solution.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> getAlternates(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; i += 2) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+}
