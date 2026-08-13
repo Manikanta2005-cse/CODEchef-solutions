473bad86519ff6089e7c24c27011e0645e058c30
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-08-13 11:26:57 +0530

    ✅ POLMDB52C | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/medium/POLMDB52C/solution.cpp
@@ -0,0 +1,16 @@
+
+
+print("Step 3: Multikey Index");
+
+// TODO: Create index on array field
+db.orders.createIndex({
+  // write your code here
+});
+
+// TODO: Query array field
+db.orders.find({
+  // write your code here
+}).forEach(doc => printjson(doc));
+  items: 1
+
+  items: "Burger"
