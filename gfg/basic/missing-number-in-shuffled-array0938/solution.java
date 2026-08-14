dba7139b2949da16ac9062dce679ccf436a5cfa6
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-08-14 23:09:50 +0530

    ✅ Missing in Another Shuffled Array | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-14

--- gfg/basic/missing-number-in-shuffled-array0938/solution.java
+++ gfg/basic/missing-number-in-shuffled-array0938/solution.java
@@ -1,14 +1,22 @@
+import java.util.HashMap;
+
 class Solution {
     public int findMissing(int[] arr1, int[] arr2) {
-        int missing = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int num : arr1) {
-            missing ^= num;
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         for (int num : arr2) {
-            missing ^= num;
+            map.put(num, map.get(num) - 1);
         }
-        return missing;
+        for (int key : map.keySet()) {
+            if (map.get(key) > 0) {
+                return key;
+            }
+        }
+
+        return -1;
     }
 }
