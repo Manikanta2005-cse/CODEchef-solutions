7e418623556fa6a439322770ba4e3fddc80b4d47
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-08-14 23:02:00 +0530

    ✅ Remove Duplicates from Unsorted | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ gfg/basic/remove-duplicates-from-unsorted-array4141/solution.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+class Solution {
+    static ArrayList<Integer> removeDuplicate(int arr[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            if (!seen.contains(num)) {
+                seen.add(num);
+                result.add(num);
+            }
+        }
+
+        return result;
+    }
+}
