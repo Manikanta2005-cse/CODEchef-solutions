da81b1f35d5669833805790724bf1cb27fce4ac7
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-08-15 21:33:56 +0530

    ✅ DSAAGP36 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-15

--- codechef/medium/DSAAGP36/solution.java
+++ codechef/medium/DSAAGP36/solution.java
@@ -5,25 +5,27 @@ import java.io.*;
 class Codechef {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
         int t = sc.nextInt();
         
         while (t-- > 0) {
             int n = sc.nextInt();
             int[] arr = new int[n];
-            
+            int max_element = 0;
             for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
+                if (arr[i] > max_element) {
+                    max_element = arr[i];
+                }
             }
             
-            // Brute force: nested loops to count frequency of each element
+            int[] hash = new int[max_element + 1];
+            
             for (int i = 0; i < n; i++) {
-                int count = 0;
-                for (int j = 0; j < n; j++) {
-                    if (arr[i] == arr[j]) {
-                        count++;
-                    }
-                }
-                System.out.print(count + " ");
+                hash[arr[i]]++;
+            }
+            for (int i = 0; i < n; i++) {
+                System.out.print(hash[arr[i]] + " ");
             }
             System.out.println();
         }
