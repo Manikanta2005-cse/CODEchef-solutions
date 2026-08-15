ef3e6f28bdfbd09d205441d5dfc4500f5350e1de
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-08-15 21:37:21 +0530

    ✅ HSH10 - Rating 932 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-15

--- codechef/easy/HSH10---Rating-932/solution.java
+++ codechef/easy/HSH10---Rating-932/solution.java
@@ -1,39 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    static final int M = 999983;
-    static final int MX = 1000000000;
-    static int[] Hash = new int[M];
-
-    // Hash function handling long and negative values safely
-    static int f(long x) {
-        int hash = (int) (x % M);
-        if (hash < 0) {
-            hash += M;
-        }
-        return hash;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        if (!scanner.hasNextInt()) return;
         int n = scanner.nextInt();
-        
-        long[] a = new long[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextLong();
-        }
 
-        long ans = 0; 
+        int[] arr = new int[n];
+        Map<Integer, Integer> mp = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            if (a[i] > -31623 && a[i] < 31623) {
-                ans += Hash[f(a[i] * a[i])];
-            }
-            Hash[f(a[i])]++;
+            arr[i] = scanner.nextInt();
+            mp.put(arr[i], i);
         }
 
-        System.out.println(ans);
-        scanner.close();
+        for (int i = 0; i < n; i++) {
+            System.out.print(mp.get(arr[i]) + " ");
+        }
+        System.out.println();
     }
 }
