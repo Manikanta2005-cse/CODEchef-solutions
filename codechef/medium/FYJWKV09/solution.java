249b604b2fe5c00ee2ab0feafebb04759fb189f8
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-08-15 22:05:28 +0530

    ✅ FYJWKV09 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-15

---
+++ codechef/medium/FYJWKV09/solution.java
@@ -0,0 +1,36 @@
+class InvalidEmployeeIdException extends Exception {
+    public InvalidEmployeeIdException(String message) {
+        super(message);
+    }
+}
+
+class Employee {
+    private String employeeId;
+
+    public Employee(String employeeId) throws InvalidEmployeeIdException {
+        if (!isValidEmployeeId(employeeId)) {
+            throw new InvalidEmployeeIdException("Invalid Employee ID format. Must be EMP-NNNN");
+        }
+        this.employeeId = employeeId;
+    }
+
+    private boolean isValidEmployeeId(String employeeId) {
+        return employeeId.matches("EMP-\\d{4}");
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Employee employee1 = new Employee("EMP-1234");
+            System.out.println("Employee 1 ID: " + employee1.getEmployeeId());
+
+            Employee employee2 = new Employee("EMP-ABCD"); // Invalid format
+            System.out.println("Employee 2 ID: " + employee2.getEmployeeId()); // This line won't be executed if the exception is thrown
+        } catch (InvalidEmployeeIdException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
