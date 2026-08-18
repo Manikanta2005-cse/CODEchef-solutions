d17dba7cae6d777bf8d4077617a7535108366046
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-08-18 21:39:11 +0530

    ✅ Majority Element | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ gfg/medium/majority-element-1587115620/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    int majorityElement(int arr[]) {
+        int candidate = -1;
+        int count = 0;
+
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > arr.length / 2) {
+            return candidate;
+        }
+
+        return -1;
+    }
+}
