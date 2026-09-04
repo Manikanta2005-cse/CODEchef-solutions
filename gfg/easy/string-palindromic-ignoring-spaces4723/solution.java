a9b55040645db0ff3dc8ba5d4711798e1e7544fc
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-04 20:26:49 +0530

    ✅ Palindrome Sentence | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ gfg/easy/string-palindromic-ignoring-spaces4723/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalinSent(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char chLeft = s.charAt(left);
+            char chRight = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(chLeft)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(chRight)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(chLeft) != Character.toLowerCase(chRight)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
