24af4fd8dd8471f8d9ee5a32acc3942af0e05f70
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-04 20:40:47 +0530

    ✅ CamelCase | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ hackerrank/easy/camelcase/solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+class Result {
+
+    public static int camelcase(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        int count = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
