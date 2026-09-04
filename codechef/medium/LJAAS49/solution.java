cc9e6cf041dedf7c92e4be14c91cbf0ef156cb29
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-04 21:05:57 +0530

    ✅ LJAAS49 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/medium/LJAAS49/solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+Scanner sc=new Scanner(System.in);
+int radius=sc.nextInt();
+double area=3.14*radius*radius;
+System.out.println(area);
+	}
+}
