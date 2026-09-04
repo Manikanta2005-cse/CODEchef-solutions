b39daccc0074bde4b341cc618c6c3029c26f86f7
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-04 20:48:43 +0530

    ✅ USRJ1AV2 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/medium/USRJ1AV2/solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Solution as follows
+		Scanner read = new Scanner(System.in);
+		int num = read.nextInt();
+		System.out.println(num * num);
+	}
+}
