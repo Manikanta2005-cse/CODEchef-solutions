dc3451cb2cb7b8a1b5ef668b6b9dfb88aa66f08c
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-04 21:07:02 +0530

    ✅ USRJ6V2 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/medium/USRJ6V2/solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Update the blank in the code given below
+		Scanner read = new Scanner(System.in);
+		int mile = read.nextInt();
+		double km = 1.60 * mile;
+		System.out.println(km);
+	}
+}
