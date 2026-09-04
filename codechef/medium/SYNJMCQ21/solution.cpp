b80c09672333a30fafa9a012f86ec56409eaf006
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-04 20:53:13 +0530

    ✅ SYNJMCQ21 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/medium/SYNJMCQ21/solution.cpp
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// Update the blank in the code below
+		Scanner read = new Scanner(System.in);
+		String x = read.nextLine();
+		System.out.println("Hello " + x);
+	}
+}
