829d7042a6aae1db7eee06c81c9bf83bb77702db
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-05 21:29:18 +0530

    ✅ Run Length Encoding | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ gfg/easy/run-length-encoding/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static String encode(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            while (i + 1 < n && s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            sb.append(s.charAt(i)).append(count);
+        }
+
+        return sb.toString();
+    }
+}
