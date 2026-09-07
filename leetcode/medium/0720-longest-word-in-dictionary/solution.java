7462e575b897c276612d8531d77238b9942b4d6a
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-07 21:56:41 +0530

    ✅ Longest Word in Dictionary | Medium | Java | Runtime: 18 ms (25.35%) | Memory: 46.4 MB | 2026-09-07

---
+++ leetcode/medium/0720-longest-word-in-dictionary/solution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+        Set<String> built = new HashSet<>();
+        String result = "";
+
+        for (String word : words) {
+            if (word.length() == 1 || built.contains(word.substring(0, word.length() - 1))) {
+                built.add(word);
+                if (word.length() > result.length()) {
+                    result = word;
+                }
+            }
+        }
+
+        return result;
+    }
+}
