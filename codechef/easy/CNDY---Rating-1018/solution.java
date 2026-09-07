94286e269b5c759fc4b74d1d033a3baa49d0c8d9
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-07 22:01:51 +0530

    ✅ CNDY - Rating 1018 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/easy/CNDY---Rating-1018/solution.java
+++ codechef/easy/CNDY---Rating-1018/solution.java
@@ -2,18 +2,18 @@ import java.util.*;
 import java.io.*;
 
 class Codechef {
-    public static void main(String[] args) throws java.lang.Exception {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         if (!sc.hasNextInt()) return;
         int t = sc.nextInt();
         while (t-- > 0) {
             int n = sc.nextInt();
-            HashMap<Integer, Integer> map = new HashMap<>();
+            Map<Integer, Integer> freq = new HashMap<>();
             boolean possible = true;
             for (int i = 0; i < 2 * n; i++) {
-                int a = sc.nextInt();
-                map.put(a, map.getOrDefault(a, 0) + 1);
-                if (map.get(a) > 2) {
+                int val = sc.nextInt();
+                freq.put(val, freq.getOrDefault(val, 0) + 1);
+                if (freq.get(val) > 2) {
                     possible = false;
                 }
             }
@@ -23,6 +23,5 @@ class Codechef {
                 System.out.println("No");
             }
         }
-        sc.close();
     }
 }
