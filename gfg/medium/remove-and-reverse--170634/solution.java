938f29d83fec9d2689078d7e99d4efb9dd4d2cf7
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-10 20:57:49 +0530

    ✅ Remove and Reverse | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-10

---
+++ gfg/medium/remove-and-reverse--170634/solution.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    String removeReverse(String S) {
+        int[] freq = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            freq[S.charAt(i) - 'a']++;
+        }
+
+        int left = 0, right = S.length() - 1;
+        boolean dir = true; 
+        int removeCount = 0;
+        char[] sArr = S.toCharArray();
+
+        while (left <= right) {
+            if (dir) {
+                char ch = sArr[left];
+                if (freq[ch - 'a'] > 1) {
+                    freq[ch - 'a']--;
+                    sArr[left] = '#';
+                    dir = !dir;
+                    removeCount++;
+                }
+                left++;
+            } else {
+                char ch = sArr[right];
+                if (freq[ch - 'a'] > 1) {
+                    freq[ch - 'a']--;
+                    sArr[right] = '#';
+                    dir = !dir;
+                    removeCount++;
+                }
+                right--;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch : sArr) {
+            if (ch != '#') {
+                sb.append(ch);
+            }
+        }
+
+        if (removeCount % 2 != 0) {
+            sb.reverse();
+        }
+
+        return sb.toString();
+    }
+}
