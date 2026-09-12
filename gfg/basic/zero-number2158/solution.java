bc1aadb14489f8f0e7725c1d0f8fdebff6c9b4d8
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-12 21:29:36 +0530

    ✅ Duck Number | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ gfg/basic/zero-number2158/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    static boolean check_duck(String num) {
+        if (num.charAt(0) == '0') {
+            return false;
+        }
+        for (int i = 1; i < num.length(); i++) {
+            if (num.charAt(i) == '0') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
