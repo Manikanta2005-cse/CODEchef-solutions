7a3b66cc9b6afcbbdc1259fbd283e51eed6d13f7
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-12 21:57:50 +0530

    ✅ TWOSUMM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-12

--- codechef/medium/TWOSUMM/solution.java
+++ codechef/medium/TWOSUMM/solution.java
@@ -1,34 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        if (!sc.hasNextInt()) return;
-        
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+public static int[] findPair(int[] nums, int target) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[]{map.get(complement), i};
         }
-        int target = sc.nextInt();
-
-        int[] result = findPair(nums, target);
-        if (result.length == 2) {
-            System.out.println(result[0] + " " + result[1]);
-        }
+        map.put(nums[i], i);
     }
-
-    public static int[] findPair(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-            map.put(nums[i], i);
-        }
-        return new int[] {};
-    }
+    return new int[]{}; 
 }
