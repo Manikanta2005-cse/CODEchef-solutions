54ceb9629d783626737c5191cde28639c5cc65bd
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-13 21:27:18 +0530

    ✅ String Rotated by 2 Places | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ gfg/easy/check-if-string-is-rotated-by-two-places-1587115620/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static boolean isRotated(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        if (s1.length() < 2) {
+            return s1.equals(s2);
+        }
+
+        int n = s1.length();
+
+        String clockRot = s1.substring(n - 2) + s1.substring(0, n - 2);
+        String antiClockRot = s1.substring(2) + s1.substring(0, 2);
+
+        return s2.equals(clockRot) || s2.equals(antiClockRot);
+    }
+}
