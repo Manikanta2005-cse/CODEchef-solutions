c5f70e1dad5dda91b83589c2cc139e5a29e360c7
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-13 21:50:28 +0530

    ✅ Palindrome Numbers in a Range | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ gfg/easy/print-all-palindromes-in-the-given-range/solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> printPalindromes(int m, int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = m; i <= n; i++) {
+            if (isPalindrome(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    private boolean isPalindrome(int num) {
+        int original = num;
+        int reversed = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        return original == reversed;
+    }
+}
