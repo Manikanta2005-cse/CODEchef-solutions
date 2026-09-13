329e59dda20cb6b2a58fb4e1274a78d26e597887
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-13 21:42:20 +0530

    ✅ Toggle Case | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ gfg/easy/toggle-case/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String toggleCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else if (Character.isLowerCase(c)) {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
