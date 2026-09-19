35d09bfa8caf94d84a6cd2e6d689130282a6a956
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-19 22:36:56 +0530

    ✅ LEARNJSP84 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-19

---
+++ codechef/medium/LEARNJSP84/solution.js
@@ -0,0 +1,11 @@
+// Debug/Fix the given function
+
+function solve(num1, num2) {
+  let isEqual = "";
+  if(num1 === num2) {
+     isEqual = "Equal";
+  } else {
+     isEqual = "Not Equal";
+  }
+  console.log(isEqual);
+}
