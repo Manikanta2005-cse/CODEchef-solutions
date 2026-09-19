0856304f1c2237170d3bfa0dd7df99bf61b9310b
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-19 21:35:52 +0530

    ✅ Two Sum | Easy | Java | Runtime: 8 ms (46.52%) | Memory: 47.1 MB | 2026-09-19

--- leetcode/easy/0001-two-sum/solution.java
+++ leetcode/easy/0001-two-sum/solution.java
@@ -2,31 +2,28 @@ import java.util.Arrays;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        int[][] numWithIndex = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
+        int[][] numWithIndex = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
             numWithIndex[i][0] = nums[i];
             numWithIndex[i][1] = i;
         }
-        
+
         Arrays.sort(numWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
-        
+
         int left = 0;
-        int right = n - 1;
-        
+        int right = nums.length - 1;
+
         while (left < right) {
             int sum = numWithIndex[left][0] + numWithIndex[right][0];
-            
             if (sum == target) {
-                return new int[]{numWithIndex[left][1], numWithIndex[right][1]};
+                return new int[] { numWithIndex[left][1], numWithIndex[right][1] };
             } else if (sum < target) {
                 left++;
             } else {
                 right--;
             }
         }
-        
-        return new int[]{};
+
+        return new int[] {};
     }
 }
