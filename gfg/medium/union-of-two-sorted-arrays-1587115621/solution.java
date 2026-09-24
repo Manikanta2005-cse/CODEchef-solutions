88ad3ba958fad40409a4cbe5a07336e186a6e399
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-24 20:26:57 +0530

    ✅ Union of 2 Sorted Arrays | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-24

--- gfg/medium/union-of-two-sorted-arrays-1587115621/solution.java
+++ gfg/medium/union-of-two-sorted-arrays-1587115621/solution.java
@@ -4,21 +4,25 @@ class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
         ArrayList<Integer> result = new ArrayList<>();
         int i = 0, j = 0;
-        int n = a.length, m = b.length;
+        int n = a.length;
+        int m = b.length;
 
         while (i < n && j < m) {
-            if (a[i] <= b[j]) {
+            if (a[i] < b[j]) {
                 if (result.isEmpty() || result.get(result.size() - 1) != a[i]) {
                     result.add(a[i]);
                 }
-                if (a[i] == b[j]) {
-                    j++;
-                }
                 i++;
-            } else {
+            } else if (b[j] < a[i]) {
                 if (result.isEmpty() || result.get(result.size() - 1) != b[j]) {
                     result.add(b[j]);
                 }
+                j++;
+            } else {
+                if (result.isEmpty() || result.get(result.size() - 1) != a[i]) {
+                    result.add(a[i]);
+                }
+                i++;
                 j++;
             }
         }
