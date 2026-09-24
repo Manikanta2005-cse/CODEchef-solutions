bcc410fd85f95765c55279c0de5be81cee58a01a
Manikanta Darapureddi <manikantadarapureddi2005@gmail.com>
2026-09-24 20:52:25 +0530

    ✅ Flipping an Image | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 45.5 MB | 2026-09-24

---
+++ leetcode/easy/0832-flipping-an-image/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left <= right) {
+                if (image[i][left] == image[i][right]) {
+                    int val = image[i][left] ^ 1;
+                    image[i][left] = val;
+                    image[i][right] = val;
+                }
+                left++;
+                right--;
+            }
+        }
+        return image;
+    }
+}
